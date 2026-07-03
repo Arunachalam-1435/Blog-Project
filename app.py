c78cbf6e0201e097b47735d201c326f0b8c774e5
Arunachalam-1435 <karunachalam120@gmail.com>
2026-07-03 15:38:11 +0530

    Create and show posts feature added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy import select
 from flask_migrate import Migrate
-from models import db
+from models import db, Posts
 from dotenv import load_dotenv
+from datetime import timezone
+from zoneinfo import ZoneInfo
 import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
@@ -12,12 +14,37 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# custom filter for timezone conversion
+@app.template_filter('local_timezone')
+def local_timezone(dt, tz_name='Asia/Kolkata'):
+    if not dt:
+        return ''
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=timezone.utc)
+    local_dt = dt.astimezone(ZoneInfo(tz_name))
+    return local_dt.strftime('%Y-%m-%d %H:%M')
+
 @app.route('/')
 def main():
-    return render_template('index.html', heading="Welcome all!")
+    all_posts = db.session.execute(select(Posts)).scalars().all()
+    if not all_posts:
+        all_posts = ""
+    return render_template('index.html', heading="Welcome all!", all_posts=all_posts)
 
-@app.route('/post')
+@app.route('/post', methods=['GET', 'POST', 'DELETE'])
 def post():
+    if request.method == "POST":
+        topic = request.form.get('topic')
+        content = request.form.get('body')
+        new_post = Posts(title = topic, body = content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('main', msg='Post created Successfully'))
+    elif request.method == "DELETE":
+        row = db.session.get(Posts, 1)
+        db.session.delete(row)
+        db.session.commit()
+        return "Post deleted successfully"
     return render_template('post.html', heading='Create a Post')
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -6,7 +6,8 @@ class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.Date, nullable=False, default=lambda: datetime.now(timezone.utc))
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+
 
     def __repr__(self):
         return f"<Post {self.id} {self.title}>"
